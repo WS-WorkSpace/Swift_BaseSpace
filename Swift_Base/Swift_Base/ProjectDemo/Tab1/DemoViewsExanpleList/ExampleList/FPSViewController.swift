e985232875e9967d40ff3404e73436ea5defb2eb
wangshuang <hualei_wang@163.com>
2023-08-10 17:09:36 +0800

    fps

--- Swift_Base/Swift_Base/ProjectDemo/Tab1/DemoViewsExanpleList/ExampleList/FPSViewController.swift
+++ Swift_Base/Swift_Base/ProjectDemo/Tab1/DemoViewsExanpleList/ExampleList/FPSViewController.swift
@@ -5,25 +5,43 @@
 //  Created by 王爽 on 2023/8/10.
 //
 
+import SnapKit
 import UIKit
-
 class FPSViewController: BaseViewController {
+//    private lazy var fpsButton = UIButton().then{
+//        $0.backgroundColor = .yellow
+//
+//    }
+    lazy var mButton: UIButton = {
+        let title = "这是UIButton"
+        let titleColor = RD_RandomColor()
+
+        let button = UIButton(frame: .zero)
+        button.frame = .zero
+        button.titleLabel?.font = RD_Font(16)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
+        button.setTitleColor(titleColor.withAlphaComponent(0.5), for: .highlighted)
+        button.setTitleColor(titleColor.withAlphaComponent(0.5), for: .disabled)
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = RD_RandomColor().cgColor
+        button.backgroundColor = RD_RandomColor()
+        button.addTarget(self, action: #selector(self.fpsClickButton(btn:)), for: .touchUpInside)
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(CGSizeMake(100, 50))
+        }
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.mButton.backgroundColor = .yellow
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func fpsClickButton(btn:UIButton) {
+        Lg.log("fpsClickButton")
     }
-    */
-
 }
