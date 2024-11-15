59abd9bef8191b397db3599b9d3da66f73855b8a
wangshuang <hualei_wang@163.com>
2024-11-15 23:11:53 +0800

    更新tab2

--- Swift_Base/Swift_Base/ProjectDemo/Tab1/OneTabViewController.swift
+++ Swift_Base/Swift_Base/ProjectDemo/Tab1/OneTabViewController.swift
@@ -14,58 +14,17 @@ class OneTabViewController: BaseTableViewController {
         self.configTableView()
 
         view.backgroundColor = .white
-        configModelArr = ["1", "基本控件Demo", "网络请求Demo", "4", "5", "6", "7", "8"]
-        var vcArray = ["1", "DemoListViewController", "NetWorkingListViewController", "4", "5", "6", "7", "8"]
+        configModelArr = ["基本控件Demo", "", "网络请求Demo", "4", "5", "6", "7", "8"]
+        let vcArray = ["DemoListViewController", "", "NetWorkingListViewController", "4", "5", "6", "7", "8"]
 
-        let item1 = UIBarButtonItem.RD_imageItem(imageName: navRightImage ?? ("tab5", "tab5_select"), target: self, action: #selector(self.demoListVC))
-        let item2 = UIBarButtonItem.RD_textItem(title: "NetWoring", titleColor: .black, target: self, action: #selector(self.netWoringMethod))
-        navigationItem.rightBarButtonItems = [item1, item2]
 
-        let header = SelfRefreshGifHeader(refreshingBlock: self.loadNewData)
-        header.beginRefreshing()
-        mTableView.mj_header = header
-
-        mTableView.mj_footer = SelfRefreshBackGifFooter(refreshingBlock: self.loadMoreData)
-
-        // MARK: -
-
-        self.clickCellBlock = { (index: IndexPath, str: String) in
-            var vc = NSString.RD_VC_ClassFromString(vcArray[index.row])
+        // MARK: - 点击cell的closure返回
+        self.clickCellBlock = { (index: IndexPath, _: String) in
+            let vc = NSString.RD_VC_ClassFromString(vcArray[index.row])
             if let _vc = vc {
                 self.navigationController?.pushViewController(_vc, animated: true)
             }
         }
     }
-
-    @objc func netWoringMethod() {
-        self.navigationController?.pushViewController(NetWorkingListViewController(), animated: true)
-    }
-
-    @objc func demoListVC() {
-        self.navigationController?.pushViewController(DemoListViewController(), animated: true)
-    }
-
-    func loadNewData() {
-        sleep(1)
-        self.mTableView.mj_header?.endRefreshing()
-    }
-
-    func loadMoreData() {
-        sleep(1)
-        self.mTableView.mj_footer?.endRefreshing()
-    }
 }
 
-//enum RunTime {
-//    static func swiftClassFromString(className: String) -> UIViewController? {
-//        // static let appName = Bundle.main.infoDictionary![kCFBundleNameKey as String] ?? ""
-//
-//        let preStr = Bundle.main.infoDictionary?["CFBundleExecutable"] as? String
-//        if var _preStr = preStr {
-//            let vcClass = NSClassFromString(_preStr  + "." + className) as? UIViewController.Type
-//            let vc = vcClass?.init()
-//            return vc
-//        }
-//        return nil
-//    }
-//}
--- Swift_Base/Swift_Base/ProjectDemo/Tab2/TwoTabViewController.swift
+++ Swift_Base/Swift_Base/ProjectDemo/Tab2/TwoTabViewController.swift
@@ -7,23 +7,58 @@
 
 import UIKit
 
-class TwoTabViewController: UIViewController {
-
+class TwoTabViewController: BaseTableViewController {
+    var page = 1
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configTableView()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        configModelArr = ["网络请求Demo", "2", "3", "4", "5", "6", "7", "8"]
+        let vcArray = ["NetWorkingListViewController", "2", "3", "4", "5", "6", "7", "8"]
+
+        // MARK: - BarIetms设置
+
+        let item1 = UIBarButtonItem.RD_imageItem(imageName: navRightImage ?? ("tab5", "tab5_select"), target: self, action: #selector(self.demoListVC))
+        let item2 = UIBarButtonItem.RD_textItem(title: "NetWoring", titleColor: .black, target: self, action: #selector(self.netWoringMethod))
+        navigationItem.rightBarButtonItems = [item1, item2]
+
+        // MARK: - 下拉,上拉刷新设置
+
+        let header = SelfRefreshGifHeader(refreshingBlock: self.loadNewData)
+        header.beginRefreshing()
+        mTableView.mj_header = header
+        mTableView.mj_footer = SelfRefreshBackGifFooter(refreshingBlock: self.loadMoreData)
+
+        // MARK: - 点击cell的closure返回
+
+        self.clickCellBlock = { (index: IndexPath, _: String) in
+            let vc = NSString.RD_VC_ClassFromString(vcArray[index.row])
+            if let _vc = vc {
+                self.navigationController?.pushViewController(_vc, animated: true)
+            }
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - BarIetms方法
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func netWoringMethod() {
+        self.navigationController?.pushViewController(NetWorkingListViewController(), animated: true)
     }
-    */
 
+    @objc func demoListVC() {
+        Toast.toast("这里未定义")
+    }
+
+    // MARK: - 下拉,上拉刷新方法
+
+    func loadNewData() {
+        sleep(1)
+        self.mTableView.mj_header?.endRefreshing()
+    }
+
+    func loadMoreData() {
+        sleep(1)
+        self.mTableView.mj_footer?.endRefreshing()
+    }
 }
