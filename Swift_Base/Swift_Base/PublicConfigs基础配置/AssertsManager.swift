a5ba9c454eb14833318abf1affaa0eae5968174c
wangshuang <hualei_wang@163.com>
2024-11-17 11:53:00 +0800

    资源管理器方法一

---
+++ Swift_Base/Swift_Base/PublicConfigs基础配置/AssertsManager.swift
@@ -0,0 +1,44 @@
+//
+//  AssertsManager.swift
+//  Swift_Base
+//
+//  Created by 王爽 on 2024/11/17.
+//
+
+import UIKit
+
+//typealias AM = AssertsManager
+//enum AssertsManager {
+//    enum string: String {
+//        case add = "添加"
+//    }
+//    enum image: String {
+//        case logo = "pic"
+//    }
+//    enum segue: String{
+//        case login_main
+//    }
+//}
+//extension UIImage {
+//    convenience init?(_ name: AssertsManager.image) {
+//        self.init(named: name.rawValue)
+//    }
+//}
+//extension UIViewController {
+//    func performSegue(withIdentifier identifier: AssertsManager.segue, sender: Any?) {
+//        performSegue(withIdentifier: identifier.rawValue, sender: sender)
+//    }
+//}
+//extension UIButton {
+//    func setTitle(_ title: AssertsManager.string, for state: UIControl.State) {
+//        setTitle(title.rawValue, for: state)
+//    }
+//}
+//
+//
+//struct temp{
+//    let img = UIImage(AssertsManager.image.logo)
+//    let btn = UIButton(type: .custom)
+////    btn.setTitle(R.string.add, for: .normal)
+////    performSegue(withIdentifier: R.segue.login_main, sender: self)
+//}
