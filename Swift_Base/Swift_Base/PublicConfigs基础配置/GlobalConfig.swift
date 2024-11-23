cc76673aac3c922639688c26ee2177ed5f301078
wangshuang <hualei_wang@163.com>
2024-11-23 15:41:50 +0800

    url

--- Swift_Base/Swift_Base/PublicConfigs基础配置/GlobalConfig.swift
+++ Swift_Base/Swift_Base/PublicConfigs基础配置/GlobalConfig.swift
@@ -20,19 +20,19 @@ public enum GlobalConfig {
 
     // 高德地图key
     public static let gGaoDeMapKey = "b00c88f69a38f58eee2a00b6ebe1abda"
-    
-    
-    ///GET 请求
-    ///"http://cx.shouji.360.cn/phonearea.php?number=13012345678"
-    ///"http://rap2api.taobao.org/app/mock/303994/test/dbbooklist"
-    ///http://cx.shouji.360.cn/phonearea.php?number=13012345678  //需要支持@"text/html"
+
+    /// GET 请求
+    /// "http://cx.shouji.360.cn/phonearea.php?number=13012345678"
+    /// 图书列表链接
+    public static let bookListURL = "http://rap2api.taobao.org/app/mock/303994/test/dbbooklist"
+    /// http://cx.shouji.360.cn/phonearea.php?number=13012345678  //需要支持@"text/html"
     // GET 请求
 //        let strURL = "https://mobi.hsxt.cn:9446/refactor/lcs/queryProvinceTree"
 //        let strURL1 = "http://rap2api.taobao.org/app/mock/303994/test/dbbooklist"
 //        let strURL2 = "/phonearea.php?"
 //        let strURL3 = "https://raw.githubusercontent.com/xiaoyouxinqing/PostDemo/master/PostDemo/Resources/PostListData_recommend_1.json"
 
-    //下载,上传 的接口查看tab4_基于AFN封装的网络请求模块
-    
+    // 下载,上传 的接口查看tab4_基于AFN封装的网络请求模块
+
     // MARK: -  UI
 }
