62bcba30ec623bdabc7f22d2ac9a0ba28ea38948
wangshuang <hualei_wang@163.com>
2024-12-22 15:07:37 +0800

    delete

--- Swift_Base/Swift_Base/Category无耦合分类/UIKit_Category/UIScreen+Extension.swift
+++ Swift_Base/Swift_Base/Category无耦合分类/UIKit_Category/UIScreen+Extension.swift
@@ -50,3 +50,21 @@ public extension UIScreen {
         }
     }
 }
+extension UIWindow {
+    static var keyWindow: UIWindow? {
+        var originalKeyWindow: UIWindow?
+        #if swift(>=5.1)
+            if #available(iOS 13, *) {
+                originalKeyWindow = UIApplication.shared.connectedScenes
+                    .compactMap { $0 as? UIWindowScene }
+                    .flatMap { $0.windows }
+                    .first(where: { $0.isKeyWindow })
+            } else {
+                originalKeyWindow = UIApplication.shared.keyWindow
+            }
+        #else
+            originalKeyWindow = UIApplication.shared.keyWindow
+        #endif
+        return originalKeyWindow
+    }
+}
