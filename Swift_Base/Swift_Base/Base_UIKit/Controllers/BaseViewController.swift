943783029fcbbc9077b5b0b762f8354303e09dc1
wangshuang <hualei_wang@163.com>
2024-12-22 14:53:43 +0800

    delete unused

--- Swift_Base/Swift_Base/Base_UIKit/Controllers/BaseViewController.swift
+++ Swift_Base/Swift_Base/Base_UIKit/Controllers/BaseViewController.swift
@@ -67,14 +67,6 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.hexColor(0xF2F4F7)
-//        edgesForExtendedLayout = UIRectEdge.bottom
-//        if responds(to: Selector(("edgesForExtendedLayout"))) {
-//            edgesForExtendedLayout = UIRectEdge(rawValue: 0)
-//        }
-
-        /// 适配
-//        ConfigVersion.configIOS11(self)
-//        ConfigVersion.configIOS15()
         configIOS11()
         configIOS15()
     }
@@ -101,31 +93,3 @@ class BaseViewController: UIViewController {
     }
 }
 
-extension BaseViewController {
-    // MARK: - iOS 适配
-
-    func configIOS11() {
-        /// 适配 iOS 11.0 ,iOS11以后，控制器的automaticallyAdjustsScrollViewInsets已经废弃，所以默认就会是YES
-        /// iOS 11新增：adjustContentInset 和 contentInsetAdjustmentBehavior 来处理滚动区域
-        if #available(iOS 11.0, *) {
-            UITableView.appearance().estimatedRowHeight = 0
-            UITableView.appearance().estimatedSectionHeaderHeight = 0
-            UITableView.appearance().estimatedSectionFooterHeight = 0
-            // 防止列表/页面偏移
-            UIScrollView.appearance().contentInsetAdjustmentBehavior = .never
-            UITableView.appearance().contentInsetAdjustmentBehavior = .never
-            UICollectionView.appearance().contentInsetAdjustmentBehavior = .never
-        } else {
-            automaticallyAdjustsScrollViewInsets = false
-        }
-    }
-
-    func configIOS15() {
-        // 适配iOS15，tableView的section设置
-        // iOS15中，tableView会给每一个section的顶部（header以上）再加上一个22像素的高度，形成一个section和section之间的间距
-        // 新增的sectionHeaderTopPadding会使表头新增一段间隙，默认为UITableViewAutomaticDimension
-        if #available(iOS 15.0, *) {
-            UITableView.appearance().sectionHeaderTopPadding = 0
-        }
-    }
-}
